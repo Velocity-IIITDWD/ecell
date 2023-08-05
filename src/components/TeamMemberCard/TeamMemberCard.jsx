5d3f5b1cc7165ba303d9039e9945793ddb1c94e8
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-08-05 15:29:23 +0530

    minor changes

--- src/components/TeamMemberCard/TeamMemberCard.jsx
+++ src/components/TeamMemberCard/TeamMemberCard.jsx
@@ -4,17 +4,17 @@ import './TeamMemberCard.css';
 const TeamMemberCard = (props) => {
   return (
     <div className="teamMemberCard-wrapper">
-        <div className="teamMember-pic">
+      <div className="teamMember-pic">
         <img src={props.data.img} alt="" />
+      </div>
+      <div className="teamMember-detail">
+        <div className="name-position">
+          <p className="teamMember-name">{props.data.name}</p>
+          <hr />
+          <p className="teamMember-position">{props.data.position}</p>
         </div>
-        <div className="teamMember-detail">
-            <div className="name-position">
-           <p className="teamMember-name">{props.data.name}</p>
-            <hr />
-            <p className="teamMember-position">{props.data.position}</p>
-            </div>
-                <div className="teamMember-profile-btn">Learn More</div>
-        </div>
+        <div className="teamMember-profile-btn">Learn More</div>
+      </div>
     </div>
 
   )
--- src/components/TeamsListContainer/TeamsListContainer.jsx
+++ src/components/TeamsListContainer/TeamsListContainer.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import './TeamsListContainer.css'
+import React, { useRef } from 'react';
+import './TeamsListContainer.css';
 import TeamMemberCard from '../TeamMemberCard/TeamMemberCard';
+import { BsCaretLeftFill, BsCaretRightFill } from 'react-icons/bs';
 
 function TeamsListContainer(props) {
+  const scrollContainerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: -100, behavior: 'smooth' });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: 100, behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className='teams-list-container'>
-        <div className='hr'/>
-        <div className='heading'>{props.heading}</div>
-        <div className='teams-list-scroll'>
-          {
-            props.ManagementTeamData.map((data)=>(
-              <div className="TeamDataCard" key={data.id}>
-                <TeamMemberCard data = {data}/>
-              </div>
-            ))
-          }
-        </div>
+      <div className='scroll-button-left' onClick={scrollLeft}>
+        <BsCaretLeftFill />
+      </div>
+      <div className='hr' />
+      <div className='heading'>{props.heading}</div>
+      <div className='teams-list-scroll' ref={scrollContainerRef}>
+          {props.ManagementTeamData.map((data) => (
+            <div className='TeamDataCard' key={data.id}>
+              <TeamMemberCard data={data} />
+            </div>
+          ))}
+      </div>
+      <div className='scroll-button-right' onClick={scrollRight}>
+        <BsCaretRightFill />
+      </div>
     </div>
-  )
+  );
 }
 
 export default TeamsListContainer;
